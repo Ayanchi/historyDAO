23ec88f3e82d6c69c51648bbb886907fdaa5d0ad
Ayanchi <ayanausub13@gmail.com>
2024-03-30 08:21:01 +0600

    fix

--- src/history_frontend/src/routes/+page.svelte
+++ src/history_frontend/src/routes/+page.svelte
@@ -6,29 +6,8 @@
 
 <main>
   <Header />
-   
-  <h1>What is the History DAO and why are we making it? </h1>
-  <br/>
-
-  <p>History is made all around us by all people throughout the world and in real time.  Having been written by a select few individuals over the ages, we’ve inherited a historical legacy that is truly wonderful, but at the same time narrow in its breadth.  With the advent of the internet we’ve entered a new age where the average person has the ability to digitally express their perspectives as they see them, and this has truly revolutionized humanity with the modern internet that we have today.  However one glaring problem still remained, and remains throughout the internet, and that is the same problem that plagued us when the Library of Alexandria was burned down, or the pyramid of Giza was defaced.  These, and many other historical events all share a very familiar narrative to one that we see often in the modern world.  That problem is censorship. The fact that the databases where all of the current internet is hosted can simply be destroyed one day, or the files deleted, or rewritten tells us that even a book is a better store of historical data than the current internet.
-  </p>
-  <br/>
-
-  <p>That is until along came the Internet Computer (IC) and made it possible to host information cryptographically on the blockchain permanently.  Having over 1400 nodes presently to secure all information on the network, it would take nothing short of nuclear armageddon for the IC database to be taken down, tampered, or hacked.  This technological breakthrough and our passion for history is why we decided to create History DAO, a Decentralized Autonomous Organization where we can collectively write history, every day, and permanently store it on a guaranteed tamperproof data cloud of the IC.  The purpose of this project will be to create as broad of a team of passionate historians as possible and attempt to collectively create a means to write history that’s available to all, and will be available for 100s of years to come.  Hopefully, we can make the DAO system go far above and beyond the current peer review process that we have in academia today, and utilize some of the revolutionary technology that we have today on the IC to change our approach to how we record history itself.  
-  </p>
-  <br/>
-
-  <p>It’s worth noting that this project is not about making money, nor will it ever be.  It is about preserving the history that we have today in as truthful a manner as possible, so that the future of humanity has real information from which it can draw real conclusions, and hopefully avoid some of the issues that we face today due to gross censorship.  For this reason, all academic DAO participants will be able to take a picture of their PhD., M.S., or B.S., upload it to the DAO, and receive History DAO tokens, which would give such individuals the ability to submit proposals, vote on other proposals, and guide the direction that the DAO takes.  To this end, 20% of the total DAO tokens will be allotted to historians and be airdropped to their NNS wallets free of charge.  10% will be allotted to the dev team, and 70% will go to anyone who invests into the DAO when we launch on the IC launchpad.  
-  </p>
-  <br/>
-
-  <p>The finances of the DAO will be simple and solely for the purpose of paying for hosting the on-chain data of History DAO.  To host data on the IC the DAO would need to buy ICP and spend it to pay for this on chain data.  It’s an automated process that wouldn’t be really expensive at all, considering that for now all data will be text-only.  This can be upgraded to include video, NFT, and other file types down the line if the DAO raises enough money from its daily auctions.  The daily auctions are the backbone of the DAO, and will be used to pay for hosting data on chain, with any left over money from the auctions being distributed to DAO holders weekly.  Each day will be auctioned via the History DAO website, with the person who bid the most being able to upload whatever text files they wish.  It’s important to note that there will be no censorship of any content, nor will there be moderation of text content.  Yes some information may be not to everyone's liking, some information may even be offensive to some, but in the pursuit of establishing a record of real history, anyone should be able to upload their own message in the manner that they deem fit.  
-  </p>
-  <br/>
-
-  <p>For now this concept of the History DAO and how it will work is just how we (devs) see it, and how we think it should run.  However, a DAO is all about giving control over to the organization and making it autonomous, so it is very likely that others will come along and help shape History DAO and make it what it needs to be for the sake of history.  For this reason, please join our official telegram (link) for now as we develop the site further, and let's exchange ideas, get to know one another, and develop the DAO together.
-  </p>
-  <br/>
+  <h1>About the DAO</h1>
+  <Accordion/>
 </main>
 
 <slot></slot>
